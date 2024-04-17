f62a707daca5ad8b363632ff3bbd16442b2fed0a
Sergio Duarte <holasoysergio.805@gmail.com>
2024-04-16 21:30:13 -0500

    CoR implementation

--- src/main/java/org/modelos/CadenaDescuento.java
+++ src/main/java/org/modelos/CadenaDescuento.java
@@ -1,4 +1,46 @@
 package org.modelos;
 
 public class CadenaDescuento {
+    private CadenaDescuento siguienteDescuento;
+    private double porcentajeDescuento;
+    private double valorMinimoAplicado;
+
+    CadenaDescuento (double porcentajeDescuento, double valorMinimoAplicado) {
+        this.porcentajeDescuento = porcentajeDescuento;
+        this.valorMinimoAplicado = valorMinimoAplicado;
+    }
+
+    public double aplicarDescuento(double valor){
+        double nuevoValor = 0;
+
+        if (valor >= valorMinimoAplicado){
+            nuevoValor = valor * (1 - porcentajeDescuento);
+        } else {
+            nuevoValor = valor;
+        }
+
+        if (siguienteDescuento != null){
+            return siguienteDescuento.aplicarDescuento(nuevoValor);
+        }
+        return nuevoValor;
+    }
+
+    public CadenaDescuento getSiguienteDescuento() {
+        return siguienteDescuento;
+    }
+    public void setSiguienteCadenaDescuento(CadenaDescuento siguienteDescuento) {
+        this.siguienteDescuento = siguienteDescuento;
+    }
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+    public void setPorcentajeDescuento(double porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+    public double getValorMinimoAplicado() {
+        return valorMinimoAplicado;
+    }
+    public void setValorMinimoAplicado(double valorMinimoAplicado) {
+        this.valorMinimoAplicado = valorMinimoAplicado;
+    }
 }
--- src/main/java/org/modelos/Main.java
+++ src/main/java/org/modelos/Main.java
@@ -1,7 +1,20 @@
 package org.modelos;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double valorInicial = 100000;
+        double nuevoValor;
+
+        // Descuento compras superiores a 50k
+        CadenaDescuento descuento1 = new CadenaDescuento(0.10, 50000);
+        // Descuento navideño compras superiores 90k
+        CadenaDescuento descuento2 = new CadenaDescuento(0.25, 90000);
+
+        descuento1.setSiguienteCadenaDescuento(descuento2);
+        nuevoValor = descuento1.aplicarDescuento(valorInicial);
+
+        JOptionPane.showMessageDialog(null, "El valor con descuentos incluidos es de " + nuevoValor + " pesos");
     }
 }
